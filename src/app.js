2223aa8e5b412e763b6433254959ae9c2d149814
andresguerrah <andres.guerrah@gmail.com>
2022-06-15 00:34:52 +0000

    tarea dominio terminada

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var punto = [".cl", ".com", ".es"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let p = 0; p < punto.length; p++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + punto[p]);
+        }
+      }
+    }
+  }
 };
